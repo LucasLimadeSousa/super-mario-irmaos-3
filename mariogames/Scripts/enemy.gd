d2916b4ac1e54954147ecfd109528c5ea065c68f
Mikidiasc <matheusdiasc33@gmail.com>
2025-09-29 08:58:24 -0300

    Correçao de erros
    inimigos atraveção o chão

--- mariogames/Scripts/enemy.gd
+++ mariogames/Scripts/enemy.gd
@@ -1,9 +1,11 @@
 class_name Enemy extends Area2D
 
+const POINTS_LABEL_SCENE = preload("res://scenes/points_label.tscn")
+
 @export var horizontal_speed: int = 20
 @export var vertical_speed:int = 100
 
-@onready var ray_cast_2d = $RayCast2d as RayCast2D
+@onready var ray_cast_2d = $RayCast2D as RayCast2D
 @onready var animated_sprite_2d = $AnimatedSprite2d as AnimatedSprite2D
 
 func _process(delta: float) -> void:
@@ -28,6 +30,10 @@ func die_from_hit():
 	var die_tween = get_tree().create_tween()
 	die_tween.tween_property(self,"position",position + Vector2(0,-25), .2)
 	die_tween.chain().tween_property(self, "position", position + Vector2(0, 500), 4)
+	
+	var points_label = POINTS_LABEL_SCENE.instantiate()
+	points_label.position = self.position +Vector2(-20, -20)
+	get_tree().root.add_child(points_label)
 	
 func _on_area_entered(area):
 	if area is Koopa and (area as Koopa).in_a_shell and (area as Koopa).horizontal_speed != 0:
--- mariogames/Scripts/player.gd
+++ mariogames/Scripts/player.gd
@@ -2,17 +2,20 @@ extends CharacterBody2D
 
 class_name Player
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 enum PlayerMode{
 	SMALL, 
 	BIG, 
 	SHOOTING
 }
 
+signal  points_scored(points: int)
+
+const POINTS_LABEL_SCENE = preload("res://scenes/points_label.tscn")
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var area_collision_shape_2d: CollisionShape2D = $Area2D/AreaCollisionShape2D
 @onready var body_collision_shape_2d: CollisionShape2D = $BodyCollisionShape2D
+@onready var area_2d = $Area2D
 
 @export_group("Locomotion")
 @export var run_speed_danping:float = 0.5
@@ -25,6 +28,11 @@ enum PlayerMode{
 @export var max_stomp_degree = 145
 @export var stomp_y_velocity = -150
 @export_group("")
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var player_mode = PlayerMode.SMALL
+var is_dead = false
+
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -48,14 +56,36 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area is Enemy:
 		handle_enemy_collision(area)
 func handle_enemy_collision(enemy : Enemy):
-	if enemy == null:
+	if enemy == null || is_dead:
 		return
 	if is_instance_of(enemy, Koopa) and (enemy as Koopa).in_a_shell:
 		(enemy as Koopa).on_stomp(global_position)
 	else:
 		var angle_of_collision = rad_to_deg(position.angle_to_point(enemy.position))
+		
 		if angle_of_collision > min_stomp_degree && max_stomp_degree > angle_of_collision:
 			enemy.die()
 			on_enemy_stomped()
+			spawn_points_label(enemy)
+		else:
+			die()
+
 func on_enemy_stomped():
 	velocity.y = stomp_y_velocity
+
+func spawn_points_label(enemy):
+	var points_label = POINTS_LABEL_SCENE.instantiate()
+	points_label.position = enemy.position + Vector2(-20, -20)
+	get_tree().root.add_child(points_label)
+	points_scored.emit(100)
+	
+func die():
+	if player_mode == PlayerMode.SMALL:
+		is_dead = true
+		animated_sprite_2d.play("small_death")
+		set_physics_process(false)
+
+	var death_tween = get_tree().create_tween()
+	death_tween.tween_property(self, "position", position + Vector2(0, -48), .5)
+	death_tween.chain().tween_property(self, "position", position + Vector2(0, 256), 1)
+	death_tween.tween_callback(func(): get_tree().reload_current_scene())
