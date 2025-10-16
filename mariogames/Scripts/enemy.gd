4ec9fcd8c18ff64831d832e10b2d416a41c1dec9
Mikidiasc <matheusdiasc33@gmail.com>
2025-10-16 08:55:29 -0300

    correçao de bugs

--- mariogames/Scripts/enemy.gd
+++ mariogames/Scripts/enemy.gd
@@ -6,6 +6,8 @@ const POINTS_LABEL_SCENE = preload("res://scenes/points_label.tscn")
 @export var vertical_speed:int = 100
 
 @onready var ray_cast_2d = $RayCast2D as RayCast2D
+@onready var ray_cast_2d_2 = $RayCast2D2 as RayCast2D
+@onready var ray_cast_2d_3 = $RayCast2D3 as RayCast2D
 @onready var animated_sprite_2d = $AnimatedSprite2d as AnimatedSprite2D
 
 func _process(delta: float) -> void:
@@ -13,6 +15,10 @@ func _process(delta: float) -> void:
 	
 	if !ray_cast_2d.is_colliding():
 		position.y += vertical_speed * delta
+	if !ray_cast_2d_2.is_colliding():
+		position.x += vertical_speed * delta
+	if !ray_cast_2d_3.is_colliding():
+		position.x -= vertical_speed * delta
 
 func die():
 	horizontal_speed = 0
